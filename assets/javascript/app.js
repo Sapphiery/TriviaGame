fc5bf70f267f06cc37d27a125970035f26374838
Kira <kira.mueller@outlook.com>
2019-06-25 23:30:17 -0700

    pseudocoded javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
+    var correct, incorrect, currentQuestion;
+    var questions = [{question: 'What are cats?',
+                      a: 'We may never know',
+                      b: 'Cosmic entities of goodness',
+                      c: 'Fuzz beasts',
+                      d: 'All of the above',
+                      right: 'd'},
+                    {question: '',
+                     a: '',
+                     b: '',
+                     c: '',
+                     d: '',
+                     right: ''}
+                    ];
+    
+    function newGame() {
+        correct = 0;
+        incorrect = 0;
+        currentQuestion = 0;
+    }
 
+    function askQuestion(question) {
+        //display question info
+        //start timer
+        //when user clicks button, evaluate answer and go to display answer view
+    } 
 
+    function answerQuestion(question, response) {
+        if (response === question.right) {
+            //display happy message
+        }
+        else {
+            //display what the user chose and the correct answer
+            //
+        }
+        //after timer, move to next question
+        //askQuestion(questions[++currentQuestion]);
+    }
+
+    newGame();
+    $('#test').text(correct + ' ' + incorrect + ' ' + currentQuestion);
 });
